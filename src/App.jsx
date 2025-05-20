c2fa70858814b9fb0de0127f838cf6a0e06a1df7
bamz-west <fabamiseadeolu@gmail.com>
2025-05-20 23:50:04 +0100

    resolving error

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import Contact from './components/Contact';
 import Features from './components/Features';
 import Footer from './components/Footer';
 import Hero from './components/Hero';
-import Navbar from './components/NavBar';
+import Navbar from './components/Navbar';
 import Story from './components/Story';
 
 const App = () => {
